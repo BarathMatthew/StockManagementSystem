072485f8cce05ff90a5214a2afe17c1922297c60
BarathMatthew <108358185+BarathMatthew@users.noreply.github.com>
2024-02-08 21:39:39 +0530

    Delete Backend directory

--- Backend/src/main/java/com/stms/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.stms.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import com.stms.bean.Product;
-
-public interface ProductRepo extends CrudRepository<Product, Long> {
-}
